7672beec73e5fe1c990952df7ec806dfe4e4805f
italo de Almeida <italo.almeida@estudante.ufjf.br>
2022-06-28 20:33:28 -0300

    adiciona vetor

---
+++ ex06.js
@@ -0,0 +1,12 @@
+const vetor = [1, 2, 3, 4];
+vetor[0] = 5;
+console.log(vetor);
+vetor.push(7);
+vetor.push(8);
+console.log(vetor);
+vetor.pop();
+console.log(vetor.pop());
+console.log(vetor);
+console.log(vetor.length);
+
+
