dfaf6b24c79960cdb5e38ed03d3a58e218a64dcd
italo de Almeida <italo.almeida@estudante.ufjf.br>
2022-06-28 19:47:35 -0300

    adiciona tipos simples

---
+++ ex02.js
@@ -0,0 +1,20 @@
+var a = 10;
+console.log(typeof a);
+
+var b = "isto é um texto";
+console.log(typeof b);
+
+var c = true;
+console.log(typeof c);
+
+var d;
+console.log(typeof d);
+
+var e = null;
+console.log(typeof e);
+
+var f = function(){};
+console.log(typeof f);
+
+
+
