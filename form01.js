257a9adee8775190cb3167dc8155a4a61fb1908b
italo de Almeida <italo.almeida@estudante.ufjf.br>
2022-07-19 20:29:13 -0300

    adiciona evento no submit

--- form01.js
+++ form01.js
@@ -1,11 +1,17 @@
 function criaElemento(cor, largura, altura) {
-    const eNovo = document.createElement("div");
-    eNovo.textContent = `${cor} ${largura} ${altura}`;
-    document.body.appendChild(eNovo);
+  const eNovo = document.createElement("div");
+  eNovo.textContent = `${cor} ${altura} ${largura}`;
+  document.body.appendChild(eNovo);
 }
 
-criaElemento(
+document.exm01.addEventListener("submit", submitListener);
+
+function submitListener(event) {
+  event.preventDefault();
+
+  criaElemento(
     document.exm01.cor.value,
     document.exm01.largura.value,
     document.exm01.altura.value
-);
+  );
+}
