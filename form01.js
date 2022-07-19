28c09c82a5dcc6adec4a43c0a02c45577602e34d
italo de Almeida <italo.almeida@estudante.ufjf.br>
2022-07-19 20:56:49 -0300

    cria lets para estado

--- form01.js
+++ form01.js
@@ -1,3 +1,9 @@
+let  estado = {
+cor: "#000000",
+altura: 0,
+largura: 0
+};
+
 function criaElemento(cor, largura, altura) {
   const eNovo = document.createElement("div");
   eNovo.textContent = `${cor} ${altura} ${largura} ${altura+largura}`;
@@ -9,11 +15,12 @@ document.exm01.addEventListener("submit", submitListener);
 
 function submitListener(event) {
   event.preventDefault();
+  estado.cor = document.exm01.cor.value,
+  estado.largura = Number(document.exm01.largura.value),    
+  estado.altura = Number(document.exm01.altura.value)
 
   criaElemento(
-    document.exm01.cor.value,
-    Number(document.exm01.largura.value),
-    Number(document.exm01.altura.value)
+      estado.cor, estado.altura, estado.largura
   );
 
   document.exm01.reset();
